c089eceb6b6b38dd3b24ea33274522608f0527a4
Your Name <matthewpaullewis@gmail.com>
2023-08-27 13:35:50 -0400

    Adding React frontend

---
+++ tradesim-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Hockey League Trade Simulator</h1>
+      <p>Start simulating trades here!</p>
+    </div>
+  );
+}
+
+
+export default App;
